833475e2d525b0af57bcf0af2fe3c65b70809cc7
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-11-06 22:29:13 -0300

    Added intents

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,9 +1,21 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.Manifest.permission.CALL_PHONE
+import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_VIEW
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,14 +23,38 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var parameterArl: ActivityResultLauncher<Intent>
+    private lateinit var callPhonePermissionArl: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
         setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityMainBinding.parameterBt.setOnClickListener {
+        parameterArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                it.data?.getStringExtra(PARAMETER_EXTRA).also {
+                    activityMainBinding.parameterTv.text = it
+                }
+            }
+        }
 
+        callPhonePermissionArl = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                callPhone(true)
+            } else {
+                Toast.makeText(this, getString(R.string.permission_required), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        activityMainBinding.apply {
+            parameterBt.setOnClickListener {
+                Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                    putExtra(PARAMETER_EXTRA, parameterTv.text)
+                    parameterArl.launch(this)
+                }
+            }
         }
     }
 
@@ -30,17 +66,35 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.openActivityMi -> {
+                Intent("OPEN_PARAMETER_ACTIVITY_ACTION").apply {
+                    putExtra(PARAMETER_EXTRA, activityMainBinding.parameterTv.text)
+                    parameterArl.launch(this)
+                }
                 true
             }
             R.id.viewMi -> {
+                val url = Uri.parse(activityMainBinding.parameterTv.text.toString())
+                Intent(ACTION_VIEW, url).apply {
+                    startActivity(this)
+                }
                 true
             }
 
             R.id.callMi -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
+                        callPhone(true)
+                    } else {
+                        callPhonePermissionArl.launch(CALL_PHONE)
+                    }
+                } else {
+                    callPhone(true)
+                }
                 true
             }
 
             R.id.dialMi -> {
+                callPhone(false)
                 true
             }
 
@@ -56,5 +110,13 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+    }
+
+    private fun callPhone(shouldCall: Boolean) {
+        startActivity(Intent(if (shouldCall) ACTION_CALL else ACTION_DIAL).apply {
+            "tel: ${activityMainBinding.parameterTv.text}".also {
+                data = Uri.parse(it)
+            }
+        })
     }
 }
